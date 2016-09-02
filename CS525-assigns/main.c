7161abdfa78f14344445c0b66de3461c9af8606d
QingYuan Song <qsong4@hawk.iit.edu>
2016-09-01 19:26:13 -0500

    Initial Commit

---
+++ CS525-assigns/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CS525-assigns
+//
+//  Created by 宋青原 on 9/1/16.
+//  Copyright © 2016 Qingyuan Song. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
