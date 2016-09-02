1ff30ded514221b769c7103da7a9ec65bd44a2c9
QingYuan Song <qsong4@hawk.iit.edu>
2016-09-02 09:29:25 -0500

    test

---
+++ CS525-assigns/test1.h
@@ -0,0 +1,14 @@
+//
+//  test1.h
+//  CS525-assigns
+//
+//  Created by 宋青原 on 9/2/16.
+//  Copyright © 2016 Qingyuan Song. All rights reserved.
+//
+
+#ifndef test1_h
+#define test1_h
+
+#include <stdio.h>
+
+#endif /* test1_h */
